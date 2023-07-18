78ed14346e56f904b082cdf3731d4c77fdc43e75
Lukasz Kulas <coolerbcm@gmail.com>
2023-07-18 21:10:09 +0200

    Zadanie: deklarowanie i wywoływanie zmiennych

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,23 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String introduction = "Czesc mam na imie Lukasz i wlasnie ucze sie podstaw Javy na kursie testera automatyzujacego w wieku ";
+        String instroductionTBC = "lat :)";
+        String startingDate = "Kurs rozpoczynam : ";
+        String endingDate = "Natomiast kurs koncze : ";
+        int myAge = 33;
+        double dateStart = 02.08;
+        double dateEnd = 22.11;
+
+        // char carClass = 'A';
+        // boolean isGoodProgrammer = true;
+
+            System.out.println(introduction);
+            System.out.println(myAge);
+            System.out.println(instroductionTBC);
+            System.out.println(startingDate);
+            System.out.println(dateStart);
+            System.out.println(endingDate);
+            System.out.println(dateEnd);
+    }
+
+}
