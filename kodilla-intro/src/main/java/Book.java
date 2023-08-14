31bf84fd82ec326bc20e5d2accfe463f3a34d17c
Coolerbcm <139633486+Coolerbcm@users.noreply.github.com>
2023-08-14 13:18:14 +0200

    Book.java
    2.7. Variables and static methods

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -5,19 +5,16 @@ public class Book {
     private Book(String author, String title) {
         this.author = author;
         this.title = title;
-        }
-    public String getAuthor()
-        {
-            return author;
-        }
-
-    public String getTitle()
-        {
-            return title;
-        }
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getTitle(){
+        return title;
+    }
     private static Book of(String author, String title) {
         return new Book(author, title);
-        }
+    }
     public static void main(String[] args) {
         Book book = Book.of("Isaac Asimov", "The Galaxy");
         System.out.println("Author: " + book.getAuthor());
