318866157d8aec270dbef411e95c60dea687a131
Lukasz Kulas <coolerbcm@gmail.com>
2023-08-14 11:02:51 +0200

    2.6. Exercises: code refactoring

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,34 +1,9 @@
-public class Application
-{
+public class Application {
+    public static void main(String[] args) {
+        String name = "Adam";
+        double age = 40.5;
+        double height = 178;
 
-    private final String name;
-    private final double age;
-    private final double height;
-
-    public Application(String name, double age, double height)
-    {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-    }
-
-    public void checkStatus()
-    {
-        if (name != null)
-        {
-            if (age > 30 && height > 160)
-            {
-                System.out.println("User is older than 30 and taller than 160cm");
-            } else
-                {
-                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-                }
-        }
-    }
-
-    public static void main(String[] args)
-    {
-        Application user = new Application("Adam", 40.5, 178);
-        user.checkStatus();
+        UserValidator.validateUser(name, age, height);
     }
 }
