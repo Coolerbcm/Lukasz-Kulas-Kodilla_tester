e9ed5b7a675ce45677d70cf89ec03707ae729ef5
Lukasz Kulas <coolerbcm@gmail.com>
2023-09-11 18:59:16 +0200

    6.. Task: order tests

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTest.java
@@ -0,0 +1,21 @@
+package com.kodilla.exception.homework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseTest {
+
+    @Test
+    public void testToGetWrongOrder (){
+        // given
+        Warehouse warehouse = new Warehouse();
+        // when
+
+        // then
+        assertThrows(OrderDoesntExistException.class, ()->warehouse.getOrder("1"));
+    }
+
+}
