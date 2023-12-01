42b825b68002efa2939204f20ae47c1df82dc244
Lukasz Kulas <coolerbcm@gmail.com>
2023-12-01 19:41:04 +0100

    updated

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -1,20 +1,34 @@
 package com.kodilla.spring.basic.spring_scopes.homework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalTime;
 
-class ClockTestSuite {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ClockTestSuite {
+
+    @Autowired
+    private Clock clock;
 
     @Test
-    public void testDifferentClocks(){
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Clock clock1 = context.getBean(Clock.class);
-        Clock clock2 = context.getBean(Clock.class);
+    public void testGetTimeWhenCalledThenReturnCurrentTime() {
+        // Arrange
+        // Clock object is already arranged and injected by Spring
 
-        Assertions.assertNotEquals(clock1.getTime(), clock2.getTime());
+        // Act
+        LocalTime result = clock.getTime();
+
+        // Assert
+        LocalTime now = LocalTime.now();
+        // We check if the time returned by clock.getTime() is within a reasonable range of the current time.
+        // This accounts for the time that may have passed between getting 'now' and 'result'.
+        assertTrue(result.isBefore(now.plusSeconds(1)) && result.isAfter(now.minusSeconds(1)),
+                "The time should be close to the current time");
     }
-
 }
